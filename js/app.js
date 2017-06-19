c40dfaa7b7c07bee4665efa0ee85fc5d182da55e
jairus rossi <jro225@uky.edu>
2017-06-18 20:31:45 -0400

    change zoom settings.  fix legend issues

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
     var map = L.map('map', {
         zoomSnap: .1,
         center: [
-            37.4, -83.4
+            37.4, -82.4
         ],
         zoom: 7,
         minZoom: 7,
@@ -15,7 +15,7 @@
     });
 
     var tiles = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+        attribution: 'Tiles &copy; Esri &mdash; Esri'
     }).addTo(map);
 
 
